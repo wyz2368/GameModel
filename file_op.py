00ea221f56386557067545924300bc8290fcad02
Yongzhao <wangyzh@umich.edu>
2019-03-07 16:30:27 -0500

    "basic dagraph"

--- file_op.py
+++ file_op.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 def mkdir(path):
     path = path.strip()
@@ -30,6 +31,20 @@ def isExist(path):
 def isInName(str,name):
     return str in name
 
+def get_json_data(json_file):
+    '''
+    Loads the data from the file as Json into a new object.
+    '''
+    with open(json_file) as data_file:
+        result = json.load(data_file)
+        return result
+
+def print_json(file_name, json_obj):
+    '''
+    Prints the given Json object to the given file name.
+    '''
+    with open(file_name, 'w') as my_file:
+        json.dump(json_obj, my_file)
 
 
 
